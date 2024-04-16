fb5c1bc60156f4c186b899ec911f0a8c0e7239ae
roshan-jha-23 <jha062305@gmail.com>
2024-04-16 11:45:17 +0530

    add navbar components and its ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
-        <h2>NavItem</h2>
-        {children}</body>
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 export default function Home() {
   return (
-   <h1 className="text-2xl text-center">Chai %or Code</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+      <h1 className="text-2xl text-center">Chai %or Code</h1>
+    </main>
   );
 }
