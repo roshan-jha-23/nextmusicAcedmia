9119e5fc82a82a4bf702501a08152d4a1cd9eac2
roshan-jha-23 <jha062305@gmail.com>
2024-04-16 12:36:49 +0530

    add hero section and fix tailwind bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <h1 className="text-2xl text-center">Chai %or Code</h1>
+      <HeroSection/>
     </main>
   );
 }
