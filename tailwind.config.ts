73e2d615d761137e8f047456900ee621f51b3404
roshan-jha-23 <jha062305@gmail.com>
2024-04-17 09:37:10 +0530

    add testimonial section and bug fix in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,7 +53,7 @@ const config: Config = {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
         scroll:
-          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+          "scroll var(--animation-duration, 30s) var(--animation-direction, forwards) linear infinite",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
