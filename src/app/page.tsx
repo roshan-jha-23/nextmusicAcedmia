ead04e0e6fa0e6ac59a45a8ebb9345a2ada543a2
roshan-jha-23 <jha062305@gmail.com>
2024-04-17 10:31:00 +0530

    add instructors sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -14,6 +15,7 @@ export default function Home() {
       <WhyChooseUs/>
       <MusicSchoolTestimonials/>
       <UpcomingWebinars/>
+      <Instructors/>
     </main>
   );
 }
